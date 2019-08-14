14084ef5bbaed985a8f36c2691a4dfb6d5469d24
Luciano Ramos <lucianoramos@MacBook-Pro-de-Luciano.local>
2019-08-14 19:22:21 +0200

    Primer commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>lucic0rp - Agencia de software</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/fontello/css/fontello.css" />
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <header>
+        <div class = "contenedor">
+            <img class = "logo-lucic0rp" src = "img/logo-lucic0rp.svg" alt = "logo-lucic0rp"/>
+            <input type = "checkbox" id = "menu-bar">
+            <label class = "icon-menu" color = "black" for = "menu-bar"></label>
+            <nav class = "menu">
+                <a class = "menu-enlaces" href = "#">Sobre nosotros</a>
+                <a class = "menu-enlaces" href = "mailto:info@lucic0rp.com">Contacto</a>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <section id = "descripcion">
+            <h2>Nuestros trabajos</h2>
+            <p>Estamos muy orgullosos de haber realizado con éxito los siguientes proyectos</p>
+        </section>
+        <section id = "portfolio">
+            <div class = "contenedor">
+                <!---LucianoRamos.es-->
+                <article class = "zoom">
+                    <a href="lucianoramos-es.html>"><img src="img/portfolio/lucianoramos.jpg" alt="lucianoramos.es"></a>
+                </article>
+                <!---Kfoam.es-->
+                <article class = "zoom">
+                    <a href = "http://kfoam.es"><img src="img/portfolio/kfoam.jpg" alt="kfoam.es"></a>
+                </article>
+                <!---RestauranteCasaLuciano.com-->
+                <article>
+                    <a href = "http://restaurantecasaluciano.com"><img src="img/portfolio/restaurantecasaluciano.jpg" alt="restaurantecasaluciano.com"></a>
+                </article>
+                <!---Thbytamarahidalgo.com-->
+                <article class = "zoom">
+                    <a href = "http://thbytamarahidalgo.com"><img src="img/portfolio/thbytamarahidalgo.jpg" alt="thbytamarahidalgo.com"></a>
+                </article>
+                <!---Equipamientoede.com-->
+                <article>
+                    <a href = "http://equipamientoede.com"><img src="img/portfolio/equipamientoede.jpg" alt="equipamientoede.com"></a>
+                </article>
+                
+            </div>
+        </section>
+        <footer>
+            <div class="contenedor">
+                <p class = "copyright">lucic0rp &copy; 2019</p>
+                <div class = "sociales">
+                    <a href="https://es-es.facebook.com/lucic0rp/" target="_blank" class = "icon-facebook"></a>
+                    <a href="https://twitter.com/lucic0rp" target="_blank" class = "icon-twitter"></a>
+                    <a href="https://www.instagram.com/lucic0rp" target="_blank" class = "icon-instagram"></a>
+                </div>
+            </div>
+        </footer>
+    </main>
+</body>
+</html>
